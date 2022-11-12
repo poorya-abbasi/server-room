a9b6b8c33a4c1257abc8a64edb9ab464c882da4c
Poorya <hey@pooryaa.com>
2022-11-12 13:46:10 +0330

    feat(sensors): add sensor registration endpoint

--- src/routes/sensors.ts
+++ src/routes/sensors.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import { z } from "zod";
 const router = express.Router();
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
@@ -6,6 +7,26 @@ const prisma = new PrismaClient();
 router.get("/", async (req: Request, res: Response) => {
     const sensors = await prisma.sensors.findMany();
     res.status(200).json(sensors);
+});
+
+router.post("/", async (req: Request, res: Response) => {
+    const validation = z
+        .object({
+            name: z.string().min(1, { message: "Name is required" }),
+            type: z.enum(["electric", "condition", "event"]),
+        })
+        .safeParse(req.body);
+    if (!validation.success) {
+        return res.status(422).json(validation.error);
+    }
+    const { name, type } = req.body;
+    const sensor = await prisma.sensors.create({
+        data: {
+            name,
+            type,
+        },
+    });
+    res.status(200).json(sensor);
 });
 
 export default router;
