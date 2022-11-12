560d225de70b52598e166ef157d8c94fb86b8c12
Poorya <hey@pooryaa.com>
2022-11-12 14:18:17 +0330

    feat(sensors): add sensor by id

--- src/routes/sensors.ts
+++ src/routes/sensors.ts
@@ -29,4 +29,49 @@ router.post("/", async (req: Request, res: Response) => {
     res.status(200).json(sensor);
 });
 
+router.put("/:id", async (req: Request, res: Response) => {
+    const idValidation = z.string().uuid().safeParse(req.params.id);
+    if (!idValidation.success) {
+        return res.status(404).json(idValidation.error);
+    }
+    const validation = z
+        .object({
+            name: z.string().min(1, { message: "Name is required" }),
+        })
+        .safeParse(req.body);
+    const { name } = req.body;
+    if (!validation.success) {
+        return res.status(422).json(validation.error);
+    }
+    const sensor = await prisma.sensors.update({
+        where: { id: req.params.id },
+        data: { name },
+    });
+    if (!sensor) {
+        return res.status(404).json({ message: "Sensor not found" });
+    } else {
+        return res.status(200).json(sensor);
+    }
+});
+
+router.get("/:id", async (req: Request, res: Response) => {
+    const idValidation = z.string().uuid().safeParse(req.params.id);
+    if (!idValidation.success) {
+        return res.status(404).json(idValidation.error);
+    }
+    const sensor = await prisma.sensors.findFirst({
+        where: { id: req.params.id },
+        include: {
+            ElecMetrics: { take: 50, orderBy: { createdAt: "desc" } },
+            ConditonMetrics: { take: 50, orderBy: { createdAt: "desc" } },
+            Events: { take: 50, orderBy: { createdAt: "desc" } },
+        },
+    });
+    if (!sensor) {
+        return res.status(404).json({ message: "Sensor not found" });
+    } else {
+        return res.status(200).json(sensor);
+    }
+});
+
 export default router;
